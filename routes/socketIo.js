7b0e5d06e2cf9b85c9559aa7481b421717f3d8fc
Nguyen Khac Tien <tienthanhphathuy@gmail.com>
2019-11-15 10:27:49 +0700

    update7

--- routes/socketIo.js
+++ routes/socketIo.js
@@ -72,46 +72,46 @@ const order = async (io) => {
   });
 }
 
-// function getdate() {
-//   var dateObj = new Date();
-//   var month = dateObj.getUTCMonth() + 1; //months from 1-12
-//   var day = dateObj.getUTCDate();
-//   var year = dateObj.getUTCFullYear();
-//   newdate = year + "-" + month + "-" + day;
-//   return newdate;
-// }
+function getdate() {
+  var dateObj = new Date();
+  var month = dateObj.getUTCMonth() + 1; //months from 1-12
+  var day = dateObj.getUTCDate();
+  var year = dateObj.getUTCFullYear();
+  newdate = year + "-" + month + "-" + day;
+  return newdate;
+}
 
-// setTimeout(() => {
-//   let date = getdate();
-//   fs.writeFileSync(`./data/bill/${date}_datcom.json`, JSON.stringify(bills), 'utf8');
-// }, 60000);
+setTimeout(() => {
+  let date = getdate();
+  fs.writeFileSync(`./data/bill/${date}_datcom.json`, JSON.stringify(bills), 'utf8');
+}, 60000);
 
-// setInterval(async () => {
-//   let date = getdate();
-//   var oder = JSON.parse(fs.readFileSync(`./data/bill/${date}_datcom.json`));
-//   if (JSON.stringify(oder) === JSON.stringify(bills)) {
-//     console.log('constant file')
-//   }
-//   else {
-//     fs.writeFileSync(`./data/bill/${date}_datcom.json`, JSON.stringify(bills), 'utf8');
-//     console.log('write file')
-//   }
-// }, 120000);
+setInterval(async () => {
+  let date = getdate();
+  var oder = JSON.parse(fs.readFileSync(`./data/bill/${date}_datcom.json`));
+  if (JSON.stringify(oder) === JSON.stringify(bills)) {
+    console.log('constant file')
+  }
+  else {
+    fs.writeFileSync(`./data/bill/${date}_datcom.json`, JSON.stringify(bills), 'utf8');
+    console.log('write file')
+  }
+}, 120000);
 
-// setInterval(async () => {
-//   var date = getdate();
-//   if (!fs.existsSync(`./data/bill/${date}_datcom.json`)) {
-//     console.log("New day");
-//     fs.writeFileSync(`./data/bill/${date}_datcom.json`, JSON.stringify({__summary:{totalPrice:0,totalProuct:0}}), 'utf8');
-//     return bills = {
-//       __summary: {
-//         totalPrice: 0, totalProuct: 0
-//       }
-//     }
-//   }
-// }, 1000);
+setInterval(async () => {
+  var date = getdate();
+  if (!fs.existsSync(`./data/bill/${date}_datcom.json`)) {
+    console.log("New day");
+    fs.writeFileSync(`./data/bill/${date}_datcom.json`, JSON.stringify({__summary:{totalPrice:0,totalProuct:0}}), 'utf8');
+    return bills = {
+      __summary: {
+        totalPrice: 0, totalProuct: 0
+      }
+    }
+  }
+}, 1000);
 setInterval(async () => {
   global.order = bills;
-}, 1000);
+}, 100);
 
 module.exports = order
